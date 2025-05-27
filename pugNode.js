81e6c12d4b6838733b9fafab1ade1aec9669d41f
KhaylaAP <155759845+KhaylaAP@users.noreply.github.com>
2025-05-27 15:39:06 +0700

    rename app.js files

--- pugNode.js
+++ pugNode.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './pages');
 
-const myRouter = require('./routes/route');
+const myRouter = require('./route/route');
 
 app.use(express.urlencoded({extended: false}));
 app.use(myRouter);
